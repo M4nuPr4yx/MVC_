a038887c27e7125e380eb8149184611bd83d6790
Emanuel Gomes <m4nupr4ix@gmail.com>
2026-05-26 14:14:06 -0300

    corrigindo

--- SistemaMVC/server.js
+++ SistemaMVC/server.js
@@ -6,6 +6,8 @@ const mysql = require('mysql2/promise')
 const pessoaRoutes = require('./src/routes/pessoasRoutes')
 const produtoRoutes = require('./src/routes/produtosRoutes')
 const calcasRoutes = require('./src/routes/calcasRoutes')
+const blusasRoutes = require('./src/routes/blusasRoutes')
+
 
 const app = express()
 app.use(cors())
@@ -14,7 +16,7 @@ app.use(express.json())
 app.use(pessoaRoutes)
 app.use(produtoRoutes)
 app.use(calcasRoutes)
-
+app.use(blusasRoutes)
 
 const PORT = process.env.PORT
 app.listen(PORT, ()=> console.log(`server em http://localhost:${PORT}`))
